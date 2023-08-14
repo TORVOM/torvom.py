2bcea350a49eabf0b27124543f4008d6a1071e3c
TORVOM <134101258+TORVOM@users.noreply.github.com>
2023-08-14 01:44:29 -0300

    Update-0.2.9.py

--- .py
+++ .py
@@ -1,8 +1,9 @@
-emloqimport socket
+import dns.resolver
+import socket
 #pip install time
 #pip install socket
-#pip insrall subprocess
-print('verção: 0.0.4')
+#pip insrall dnspython
+print('verção: 0.2.1')
 
 #>>>>>>>>>>>>>>> CORES #
 norm = '\033[m'        #
@@ -10,56 +11,86 @@ cvermelho = '\033[31m' #
 cazul = '\033[34m'     #
 cverde = '\033[32m'    #
 #=======================
+def scannerIpHost():
+        try:
+                host02 = input(f'''{cverde}
+scanner host>{norm} ''')
+                ip = socket.gethostbyname(host02)
+                print(f'\no ip do host {cverde}{host02}{norm} é {cazul}{ip}')
+        except:
+                print(f'\n{cvermelho}esse host não existe{norm}')
+                pass
 def pesquisaGoogle():
-        print(f'{cvermelho}caso não queira prencher\nnão coloque nada{cverde}')
-        Pp = str(input(f'''palavra específica
-que você quer encontrar
+        print(f'{cvermelho}caso não queira pre-encher\nalgo não coloque nada.{cverde}\n')
+        input('entendi! >ENTER<\n')
+        Pp = str(input(f'''{cverde}
+palavra específica
+que você quer encontrar.
 |
 |___|>{norm}'''))
-        print('')
-        Ps = str(input(f'''{cverde}site especifico
-que quer encontrar
+        Ps = str(input(f'''{cverde}
+site específico que
+você quer encontrar.
 |
 |___|>{norm}'''))
-        print('')
-        Pa = str(input(f'''{cverde}quer que seja em
-algum arquivo? se sim, qual?
-pdf, txt?..
+        Pa = str(input(f'''{cverde}
+tipo arqetip.
+pdf, txt, ftp?..
 |
 |___|>{norm}'''))
         print(f'''
 {cverde}copie e cole no google o texto a baixo.
 
 {cazul}"{Pp}" site:{Ps} filetype:{Pa}
-{norm}
-''')
+{norm}''')
 
 def portaIp():
-        site01 = str(input(f'''{cverde}informe o host
-|
-|___|>{norm}'''))
-        ports = [21,22,23,88,443,80,8080,8090,7777,24,25]
-        for port in ports:
-                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                reap = client.connect_ex((site01, port))
-                if reap == 0:
-                        print(f'{cverde}[+]{norm} {port} aberta\n')
-                else:
-                        print(f"{cvermelho}[-]{norm} {port} fechada\n")
-client.close()
+        hosts = str(input(f'''{cverde}
+host>{norm}'''))
+        ports = int(input(f'''{cverde}
+porta>{norm}'''))
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        code = client.connect_ex((hosts, ports))
+        if code != 0:
+                print(f'\n{cvermelho}[-]{cazul} {ports} fechada{norm}\n')
+        else:
+                print(f"\n{cverde}[+]{cazul} {ports} aberta{norm}\n")
+        client.close()
 
 
 
 while True:
         resut01 = int(input(f'''{cverde}
+
+=========================================
 [01] pesquisa avançada no google
-{cvermelho}[02]{cverde} procurar portas ips
-[03]
+[02] conecção de portas ips/hosts
+[03] trava chat
+[04] scanner host
 |
 |___|>{norm}'''))
         if resut01 == 1:
                 pesquisaGoogle()
         elif resut01 == 2:
                 portaIp()
+        elif resut01 == 3:
+                print(f'''{cvermelho}copie o texto azul
+e cole no chat que deseja trava.
+>>ATENÇÃO, VOCÊ PODE DANIFICAR
+SEU APARELHO AO COLAR NO CHAT OU APERTAR
+ENTER!<<!{norm} Aperte >ENTER< para obter
+o trava chat.
+''')
+                it1 = input(f'''{cvermelho}
+caso queira sair
+digite "S"{norm}''')
+                if it1 == 's' or it1 == 'S':
+                        print('ok...')
+                else:
+                        print(f'''{cazul}
+||**By:LEIA>z҉҉҉҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉**||
+{norm}''')
+        elif resut01 == 4:
+                scannerIpHost()
         else:
-                print(f'{cvermelho}opção não e>
+                print(f'{cvermelho}opção não existente{norm}')
