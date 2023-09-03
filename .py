a0501a9e467b64cba30316c85b273418ca7bf9a0
TORVOM <134101258+TORVOM@users.noreply.github.com>
2023-09-03 20:33:08 -0300

    Update-tv.py

--- .py
+++ .py
@@ -1,57 +1,57 @@
-import dns.resolver
-import socket
-from time import sleep
 #pip install time
 #pip install socket
 #pip install dnspython
-sleep(1)
+#import dns.resolver <- talvez não precise!
+import socket
+from time import sleep
+ve03 = 9
+sleep(1.5)
 #>>>>>>>>>>>>>>> CORES #
 norm = '\033[m'        #
 cvermelho = '\033[31m' #
 cazul = '\033[34m'     #
 cverde = '\033[32m'    #
-sist = '\033[m'        #
+sist = '\033[4;30;43m' #
 #=======================
+ve01 = 0
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> sistema #
-er = '\n' + cvermelho +'comando errado/erro' + norm + '\n'                         #
+er = '\n' + cvermelho +'comando errado/erro <01>' + norm + '\n'                           #
+ser = '\n' + cvermelho + 'comando errado/erro <02>' + norm + '\n'                         #
 #============================================
-vp1 = 0
-vp2 = 3
-vp3 = 8
-vpg = vp1 + '.' + vp2 + '.' + vp3
-print(vpg)
+ve01 = 0
+ve02 = 4
+ve03 = 7
+vercc = f'{sist}{ve01}.{ve02}.{ve03}{norm}'
+print()
+# opc 3 >
 def travaChats():
-        trv = input(f'''{cvermelho}
+        trvs = str(input(f'''{cvermelho}
 =========≈≈=============================
 ATENÇÃO: caso você aperte enter ou cole
 o texto em algum chat o seu dispositivo
-pode ser danificado. >não use para o mal<
-{cvermelho}
+pode ser danificado. >{norm}não use para o mal{cvermelho}<
 ========================================
-{cverde}caso não queira o trava
-chat digite "S"
-se quiser aperte >ENTER<
+{cverde}
 COPIE O TEXTO AZUL E COLE NO CHAT DO ALVO
-|
-|___>{norm}''')
-        if trv != 's' or trv != 'S':
+
+>ENTER<{norm}'''))
+        if trvs != 's' or trvs != 'S':
                 print(f'''{cazul}
->^<
+ ||**My:leia!>z҉҉҉҉ ؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉؜؜؜؜҈҉҈҉**||
 {norm}''')
         else:
-                try:
-                        print('ok...\n')
-                except:
-                        print(er)
+                print('ok...\n')
+# opc 4 >
 def scannerIpHost():
         try:
                 host02 = input(f'''{cverde}
 scanner host>{norm} ''')
                 ip = socket.gethostbyname(host02)
-                print(f'\no ip do host {cverde}{host02}{norm} é {cazul}{ip}')
+                print(f'\no ip do host {cverde}{host02}{norm} é {sist}{ip}{norm}')
         except:
                 print(f'\n{cvermelho}esse host não existe{norm}')
                 pass
+# opc 1 >
 def pesquisaGoogle():
         print(f'{cvermelho}caso não queira pre-encher\nalgo não coloque nada.{cverde}\n')
         input('entendi! >ENTER<\n')
@@ -60,9 +60,10 @@ palavra específica
 que você quer encontrar.
 |
 |___|>{norm}'''))
-        Ps = str(input(f'''{cverde}                                                                                                      
+        Ps = str(input(f'''{cverde}
 site específico que
-você quer encontrar.                                                                                                                                                             |
+você quer encontrar.
+|
 |___|>{norm}'''))
         Pa = str(input(f'''{cverde}
 tipo arqetip.
@@ -75,41 +76,61 @@ pdf, txt, ftp?..
 {cazul}"{Pp}" site:{Ps} filetype:{Pa}
 {norm}''')
 
+# opc 2 >
 def portaIp():
         hosts = str(input(f'''{cverde}
 host>{norm}'''))
         ports = int(input(f'''{cverde}
 porta>{norm}'''))
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        code = client.connect_ex((hosts, ports))
-        if code != 0:
-                print(f'\n{cvermelho}[-]{cazul} {ports} fechada{norm}\n')
-        else:
-                print(f"\n{cverde}[+]{cazul} {ports} aberta{norm}\n")
-        client.close()
-
-
-
+        try:
+                code = client.connect_ex((hosts, ports))
+                if code != 0:
+                        print(f'\n{cvermelho}[-]{cazul} {ports} fechada{norm}\n')
+                else:
+                        print(f"\n{cverde}[+]{cazul} {ports} aberta{norm}\n")
+                client.close()
+        except:
+                print(er)
+# opc 5 >
+def ferramentasInvestigacao():
+        print(ser)
 while True:
         resut01 = int(input(f'''{cverde}
 
+
+
+inf.=========> TORVOM
+| criador: ADM
+| verção: {vercc}{cverde}
 =========================================
-[01] pesquisa avançada no google
-[02] verificação de conecção (server)
-[03] trava chat
-[04] scanner ip host
-|
-|___|>{norm}'''))
-        if resut01 == 1:
-                pesquisaGoogle()
-        elif resut01 == 2:
-                portaIp()
-        elif resut01 == 3:
-                travaChats()
-        elif resut01 == 4:
-                scannerIpHost()
-        else:
+| [01] pesquisa avançada no google      |
+| [02] verificação de conecção (server) |
+| [03] trava chat                       |
+| [04] scanner ip host                  |
+| [05] ferramentas de investigação      |
+| [06] enceri  códigos                  |
+=========================================
+<(○-○)>
+
+
+{cazul}*{norm} '''))
+        try:
+                if resut01 == 1:
+                        pesquisaGoogle()
+                elif resut01 == 2:
+                        portaIp()
+                elif resut01 == 3:
+                        travaChats()
+                elif resut01 == 4:
+                        scannerIpHost()
+                elif resut01 == 5:
+                        ferramentasInvestigacao()
+                else:
+                        print(er)
+        except:
+                print(er)
                 try:
-                        print(er)
+                        print(ser)
                 except:
-                        print(er)
+                        pass
